3eb2ea81f7639ea53c2df24871cbd51d9e7122ec
Marius LEGOUALLEC <mlegoual@gmail.com>
2023-12-01 09:50:52 +0100

    Patient

--- src/main/java/org/example/Patient.java
+++ src/main/java/org/example/Patient.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Date;
 import java.time.*;
 
@@ -10,15 +11,14 @@ public class Patient {
     private int souhaitChambre;
     private int numService;
     private LocalDateTime heureArrive;
+    private ArrayList<Chambre> chambres;
+    private Service service;
+    private Chambre c;
     public Patient(int numSecuriteSocial,String nom, int souhaitChambre , int numService){
         this.nom = nom;
         this.numSecuriteSocial = numSecuriteSocial;
         this.souhaitChambre = souhaitChambre;
         this.numService = numService;
         heureArrive = LocalDateTime.now();
-    }
-
-    public void Admission(){
-
     }
 }
